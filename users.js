b2ae9b7e68524860f7e3ae31f3452d43f94754ef
ric <riccardo.at.work@gmail.com>
2019-02-07 01:03:25 +0100

    added show detail toggle

--- users.js
+++ users.js
@@ -18,13 +18,26 @@ function show(users) {
     let userArea = $('#users');
     userArea.empty();
     users.forEach(user => {
-        userArea.append(`<div>${user.name} of ${user.company.name} <a role = "button" class="btn btn-sm btn-info" data-id="${user.id}" href="#">DETAIL</a></div>`);
+        userArea.append(`<div>${user.name} of ${user.company.name} <a role = "button" data-id="${user.id}" class = "btn btn-small btn-info" href="#">DETAIL</a><div class ='detail' style='display:none'></div></div>`);
         userArea.append('<br/>');
     });
 }
 
 function toggleDetails(evt) {
-    console.log($(''))
-    console.log(evt.target.dataset.id);
-    $(evt.target).after("<div class ='detail'>Dettagli..</div>");
+    let detail = $(evt.target).parent().find('div.detail').first();
+    if (detail.is(':empty')){
+        fetch(`https://jsonplaceholder.typicode.com/users/${evt.target.dataset.id}`)
+        .then(resp => resp.json())
+       // .then(data => showDetails(data, detail))
+        .then(data => detail.append("ciao"))
+        .then(detail.slideToggle("slow"))
+        .catch(error => console.log(error));
+    }
+    else{
+        detail.slideToggle("slow");
+    } 
+}
+
+function showDetails(user, detailDiv) {
+    detailDiv.append(`username:  ${user.username} email:  ${user.email} street address:  ${user.address.street}`);
 }
